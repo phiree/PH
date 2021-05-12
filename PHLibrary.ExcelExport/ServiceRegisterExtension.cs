a8e3767f28abfdcc7f673e42b64a80c5d621866b
YuanFei <YuanFei@DESKTOP-FI2RCSQ>
2021-05-12 17:00:01 +0800

    feat: use serviceextension for client to inject

---
+++ PHLibrary.ExcelExport/ServiceRegisterExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PHLibrary.ExcelExport
+{
+  public static  class ServiceRegisterExtension
+    {
+        public static IServiceCollection AddPHExcelExport(this IServiceCollection services)
+        {
+            services.AddScoped<IExcelCreator,ExcelCreatorEPPlus>();
+            return services;
+        }
+    }
+}
